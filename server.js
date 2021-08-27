3f3893f833f4e2a1dba1b53b31fd10f98e5fda1d
NukaGrizz <nukagrizz@gmail.com>
2021-08-26 23:47:18 -0400

    inital files and data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { animals } = require('./data/animals');
 
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray;
